98457f04a6fcb94c43f03a5e78efb63fb2c8fef2
Justin Purnell <jpurnell@users.noreply.github.com>
2022-12-02 11:17:57 -0500

    Comparable and Identifiable Conformance

--- Sources/Classmate/Classmate.swift
+++ Sources/Classmate/Classmate.swift
@@ -4,8 +4,18 @@
 //
 //  Created by Justin Purnell on 11/22/22.
 //
+import Foundation
 
-public struct Classmate: Codable {
+public struct Classmate: Codable, Comparable, Identifiable {
+    public static func < (lhs: Classmate, rhs: Classmate) -> Bool {
+        return lhs.lastName < rhs.lastName && lhs.firstName < rhs.firstName && lhs.preferredFirst ?? "" < rhs.preferredFirst ?? ""
+    }
+    
+    public static func == (lhs: Classmate, rhs: Classmate) -> Bool {
+        return lhs.lastName == rhs.lastName && lhs.firstName == rhs.firstName && lhs.preferredFirst == rhs.preferredFirst
+    }
+    
+    public var id: UUID = UUID()
     public let firstName: String
     public let lastName: String
     public var name: String  { return "\((preferredFirst != nil) ? preferredFirst! : firstName) \(lastName)"}
@@ -17,5 +27,6 @@ public struct Classmate: Codable {
     public static func fixture(_ firstName: String = "first",_ lastName: String = "last", email: String = "", phone: String = "", address: [Address] = [.fixture()]) -> Classmate {
         return Classmate (firstName: firstName, lastName: lastName, email: email, phone: phone, addresses: address)
     }
+    
 }
 
